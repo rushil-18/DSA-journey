2bcd6264003a781138637c0caee293aa93262d52
rushil-18 <mrushil18@gmail.com>
2026-07-10 16:16:16 +0530

    loops- while, for

---
+++ c++-basics/loops/forloop.cpp
@@ -0,0 +1,88 @@
+// for loop : A for loop is just a while loop with the setup written neatly in one place.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int start = 1;  //isnt this req? - isnt
+    int i;
+    cout << "eneter a number:";
+    cin >> i;
+
+    for(int start=1 ; start<=i ; start++ ){
+        cout << start << endl;
+
+    }
+
+    return 0;
+
+    
+}
+//output:
+//eneter a number:5
+//1
+//2
+//3
+//4
+//5
+
+//exercises :
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int start = 1;  //isnt this req?
+    int i = 10;           //you dont need an extra variable for this program
+    
+
+    for(int start=1 ; start<=i ; start++ ){
+        cout << start << endl;
+
+    }
+
+    return 0;
+
+    
+}
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    
+    int i = 1;
+    
+
+    for(int start=10 ; i<=start ; start-- ){
+        cout << start << endl;
+
+    }
+
+    return 0;
+
+    
+}
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 1;
+    int b;
+    cout << "enter a number:";
+    cin >> b;
+
+    for(int c = b; a<=c ; c--){  //cause if u do for(int b )- the system wont take the user input itll be ignored
+        cout << c << endl;
+    }
+    return 0;
+}
+
+
+
--- c++-basics/loops/whileloop.cpp
+++ c++-basics/loops/whileloop.cpp
@@ -1,2 +1,104 @@
+//The while loop
 
-    
+//Syntax:
+
+//while(condition)
+//{
+    // code
+//}
+
+//Read it like English:
+
+//"While this condition is true, keep running the code."
+
+//#include<iostream>
+//using namespace std;
+
+//int main(){
+    //int i = 10;
+
+    //while (i >= 1){
+        //cout << i << endl;
+        //i--;
+
+        
+    //}
+    //return 0;
+//}
+//output : 1
+//2
+//3
+//4
+//5
+//output:10
+//9
+//8
+//7
+//6
+//5
+//4
+//3
+//2
+//1
+//MOVING UP ++
+//#include<iostream>
+//using namespace std;
+
+//int main(){
+    //int intial = 1;
+    //int limit;
+    //cout << "enter a number :";
+    //cin >> limit;
+
+    //while (intial <= limit){
+        //cout << intial << endl;
+        //intial++;
+    //}
+//return 0;
+//}
+//output : 
+//enter a number :6
+//1
+//2
+//3
+//4
+//5
+//6
+// MOVING DOWN
+//#include<iostream>
+//using namespace std;
+//int main(){
+    //int fixedvalue = 1;
+    //int uservalue;
+    //cout <<"enter a number:";
+    //cin >> uservalue;
+
+    //while(fixedvalue <= uservalue){
+        //cout << uservalue << endl;
+       // uservalue--;
+    ////}
+//}
+//output:enter a number:8
+//8
+//7
+//6
+//5
+//4
+//3
+//2
+//1
+//SKIPPING ONE : 
+//#include<iostream>
+//using namespace std;
+
+//int main(){
+    //int intial = 1;
+    //int limit;
+    //cout << "enter a number :";
+    //cin >> limit;
+
+    //while (intial <= limit){
+        //cout << intial << endl;
+        //intial +=2;
+    //}
+//return 0;
